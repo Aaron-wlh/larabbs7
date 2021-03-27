b19989ac0eed69871cd81d7e25d94ef5b4df53f6
Aaron-wlh <976724250@qq.com>
2021-03-27 05:53:23 +0000

    认证后提示

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,7 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
+Route::get('test/{a}/{b}', function(\Illuminate\Http\Request $request) {
+    dd($request->route()->parameters);
+});
 
