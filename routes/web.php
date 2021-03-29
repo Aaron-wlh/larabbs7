60663822ae89b47558617a278386747f95a2e112
Aaron-wlh <976724250@qq.com>
2021-03-29 08:04:11 +0000

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,5 @@ Route::get('test/{a}/{b}', function(\Illuminate\Http\Request $request) {
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
