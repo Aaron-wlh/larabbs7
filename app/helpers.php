6ef112919772326d1b2706670c952e869f1ed46a
Aaron-wlh <976724250@qq.com>
2021-03-29 09:46:57 +0000

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -3,5 +3,13 @@
 use \Illuminate\Support\Facades\Route;
 
 function route_class() {
-    return str_replace('-', '.', Route::currentRouteName());
+    return str_replace('.', '-', Route::currentRouteName());
+}
+
+function category_nav_active($route_name, $url_param) {
+    if (\Route::currentRouteName() == $route_name
+        && (\Route::current()->originalParameters())['category'] == $url_param
+    ) {
+        return 'active';
+    }
 }
