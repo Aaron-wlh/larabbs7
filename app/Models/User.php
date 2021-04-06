fb9a0a463f94999bffb13ff46c594358593252e8
Aaron-wlh <976724250@qq.com>
2021-04-06 10:37:04 +0000

    消息通知有新回复

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use Notifiable;
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
     use \Illuminate\Auth\MustVerifyEmail;
 
     /**
@@ -37,6 +40,18 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function notify($instance)
+    {
+        if ($this->id == Auth::id()) {
+            return;
+        }
+
+        if (method_exists($instance, 'toDatabase')) {
+            $this->increment('notification_count');
+        }
+        $this->laravelNotify($instance);
+    }
 
     public function topics()
     {
