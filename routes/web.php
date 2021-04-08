b995a5fd6c920dc0390ab693cf3c0aff577e9226
Aaron-wlh <976724250@qq.com>
2021-04-08 13:34:00 +0000

    首页话题列表

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'topicsController@index')->name('root');
 // 用户身份验证相关的路由
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
